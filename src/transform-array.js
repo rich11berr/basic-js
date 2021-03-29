37af5045ff5835abcef19f0b22017894f8f809e2
rich11berr <rinocraft.tv@gmail.com>
2021-03-30 02:38:36 +0400

    feat: finish 2 tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) === false) {
+    throw new Error();
+  }
+  let buf = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] == '--discard-next'){
+      if(i < arr.length){
+        buf.push(undefined);
+        i+=1;
+      }
+    }
+    else if( arr[i] == '--discard-prev'){
+      if(i > 0){
+        buf.pop();
+      }
+    }
+    else if( arr[i] == '--double-next'){
+      if(i<arr.length-1){
+        buf.push(arr[i+1]);
+      }
+    }
+    else if( arr[i] == '--double-prev'){
+      if(i > 0){
+        buf.push(buf[buf.length-1]);
+      }
+    }
+    else {
+      buf.push( arr[i]);
+    }
+  }
+  let result = buf.filter(function(x) {
+    return x !== undefined
+  })
+  return result;
 };
